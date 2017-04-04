9bd6863961a3a24ea21ca8b33718adc675a23826
EpicKiwi <me@epickiwi.fr>
2017-04-04 10:49:34 +0200

    Mise a jour du site

--- src/index.php
+++ src/index.php
@@ -3,39 +3,44 @@ ini_set('display_errors', 1);
 ini_set('display_startup_errors', 1);
 error_reporting(E_ALL);
 
-$pdo = new PDO('mysql:dbname=blog;host=mysql','root','myPassword');
+$pdo = new PDO('mysql:dbname=potions;host=mysql','root','myPassword');
 $pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_WARNING);
 
-$postsStatement = $pdo->query("SELECT title,content,date,users.username AS username FROM posts INNER JOIN users ON posts.userId = users.id ORDER BY posts.date DESC");
+$postsStatement = $pdo->query("SELECT * FROM Potion");
 $posts = $postsStatement->fetchAll();
 
 ?>
 <!DOCTYPE html>
 <html>
 <head>
-	<title>PHP Fitness</title>
+	<title>L'atelier de giselle</title>
+	<meta charset="utf-8">
+	<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0-alpha.6/css/bootstrap.min.css" integrity="sha384-rwoIResjU2yc3z8GV/NPeZWAv56rSmLldC3R/AZzGRnGxQQKnKkoFVhFQhNUwEyJ" crossorigin="anonymous">
 </head>
 <body>
 
-<header>
-	<h1>PHP Fitness</h1>
-	<h2>Pour garder la forme en PHP</h2>
+<div class="container" style="padding-top: 20px">
+
+<header class="jumbotron">
+	<h1 class="display-3">L'atelier de giselle</h1>
+	<h2>Vente de potions en ligne</h2>
 </header>
 
-<section>
+<section class="row">
 	<?php foreach ($posts as $post) {?>
-	<article>
-		<h1><?= $post["title"] ?></h1>
-		<div>
-			<?= $post["content"] ?>
-		</div>
-		<div>
-			<div><?= $post["date"] ?></div>
-			<div><?= $post["username"] ?></div>
-		</div>
+	<article class="col-lg-6">
+		<h1><?= $post["Nom"] ?></h1>
+		<ul>
+			<li><strong>Diluant</strong> : <?= $post["Diluant"] ?></li>
+			<li><strong>Themperature</strong> : <?= $post["Themperature"] ?></li>
+			<li><strong>Prix</strong> : <?= $post["Prix"] ?></li>
+		</ul>
+		<hr>
 	</article>
 	<?php } ?>
 </section>
+
+</div>
 
 </body>
 </html>
