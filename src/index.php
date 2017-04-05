389b6812c285871836882a8cc9710ebf96684b3e
EpicKiwi <me@epickiwi.fr>
2017-04-05 12:13:23 +0200

    Fix PDO introuvable

--- src/index.php
+++ src/index.php
@@ -1,8 +1,4 @@
 <?php
-ini_set('display_errors', 1);
-ini_set('display_startup_errors', 1);
-error_reporting(E_ALL);
-
 $pdo = new PDO('mysql:dbname=blog;host=mysql','root','myPassword');
 $pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_WARNING);
 
